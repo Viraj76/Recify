55cef9c44d73f199fb68b5453a249622a9092ce5
Viraj76 <virajg.7680@gmail.com>
2023-01-28 00:27:42 +0530

    Type Convertor class

--- app/src/main/java/com/example/recify/db/MealTypeConverter.kt
+++ app/src/main/java/com/example/recify/db/MealTypeConverter.kt
@@ -1,4 +1,20 @@
 package com.example.recify.db
 
-class MealTypeConventer {
+import androidx.core.graphics.rotationMatrix
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@TypeConverters
+class MealTypeConverter {
+    @TypeConverter
+    fun fromAnyToString(attribute:Any?):String{
+        if(attribute == null) return ""
+        return attribute.toString()
+    }
+
+    @TypeConverter
+    fun fromStringToAny(attribute: String?):Any {
+        if(attribute==null) return ""
+        return  attribute
+    }
 }
