5e3731d318e97e3e27fb4d50bef5428922327a98
Viraj76 <virajg.7680@gmail.com>
2023-01-28 00:28:30 +0530

    Created Room Data Base

--- app/src/main/java/com/example/recify/db/MealDao.kt
+++ app/src/main/java/com/example/recify/db/MealDao.kt
@@ -1,4 +1,30 @@
 package com.example.recify.db
 
+import androidx.core.view.WindowInsetsCompat.Type.InsetsType
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.recify.classes.Meal
+import com.example.recify.classes.MealList
+
+@Dao
 interface MealDao {
+    // one way to this
+    /*
+    @Insert
+    suspend fun insertMeal(meal: Meal)
+    @Update
+    suspend fun updateMeal(meal: Meal)
+    */
+
+    //Other way
+    @Insert(onConflict = OnConflictStrategy.REPLACE)  //if the data already exists then it will update id
+    suspend fun upsert(meal:Meal)
+
+    @Delete
+    suspend fun delete(meal:Meal)
+
+    @Query("SELECT * FROM mealInfo")
+    fun getAllMeals() : LiveData<List<MealList>>
+
+
 }
--- app/src/main/java/com/example/recify/db/MealDataBase.kt
+++ app/src/main/java/com/example/recify/db/MealDataBase.kt
@@ -1,4 +1,33 @@
 package com.example.recify.db
 
-class MealDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.recify.classes.Meal
+import java.security.AccessControlContext
+
+@Database(entities = [Meal::class], version = 1)
+@TypeConverters(MealTypeConverter::class)
+abstract class MealDataBase :RoomDatabase() {
+    abstract fun mealDao():MealDao
+
+    companion object{
+        @Volatile
+        var INSTANCE : MealDataBase? = null
+
+        @Synchronized
+        fun getInstance(context: Context):MealDataBase{
+            if(INSTANCE == null){
+                INSTANCE = Room.databaseBuilder(
+                    context,
+                    MealDataBase::class.java,
+                    "meal.db"
+                ).fallbackToDestructiveMigration()
+                    .build()
+            }
+            return INSTANCE as MealDataBase
+        }
+    }
 }
