c69439f58a04d69a003bcd56d60c3964883d5139
Viraj76 <virajg.7680@gmail.com>
2023-01-30 22:42:41 +0530

    Implemented delete function (left or right swipe) also made a ssnack bar (not working right now)

--- app/src/main/java/com/example/recify/Fragments/FavrouiteFragment.kt
+++ app/src/main/java/com/example/recify/Fragments/FavrouiteFragment.kt
@@ -7,11 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
 import com.example.recify.Adapter.FavoriteMealsAdapter
 import com.example.recify.R
 import com.example.recify.actvity.MainActivity
 import com.example.recify.databinding.FragmentFavrouiteBinding
 import com.example.recify.viewModel.HomeViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class FavrouiteFragment : Fragment() {
     private lateinit var  binding: FragmentFavrouiteBinding
@@ -37,6 +41,32 @@ class FavrouiteFragment : Fragment() {
         prepareRecyclerView()
         observeFavourite()
 
+        val itemTouchHelper = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.RIGHT or ItemTouchHelper.LEFT
+        ){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                viewModel.deleteMeal(favoriteMealsAdapter.differ.currentList[position])
+
+                Snackbar.make(requireView(),"Meal Deleted",Snackbar.LENGTH_LONG).setAction(
+                    "Undo",
+                    View.OnClickListener {
+                        viewModel.insertMeal(favoriteMealsAdapter.differ.currentList[position])
+                    }
+                ).show()
+
+            }
+        }
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(binding.rvFavorite)
     }
 
     private fun prepareRecyclerView() {
