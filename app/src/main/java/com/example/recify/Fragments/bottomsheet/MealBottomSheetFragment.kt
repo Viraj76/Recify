7628260f5627f97542c913b52053b8d37d749bd0
Viraj76 <virajg.7680@gmail.com>
2023-02-11 23:32:35 +0530

    Implemented setOnClickListener on BottomSheet

--- app/src/main/java/com/example/recify/Fragments/bottomsheet/MealBottomSheetFragment.kt
+++ app/src/main/java/com/example/recify/Fragments/bottomsheet/MealBottomSheetFragment.kt
@@ -1,13 +1,16 @@
 package com.example.recify.Fragments.bottomsheet
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.example.recify.Fragments.HomeFragment
 import com.example.recify.R
 import com.example.recify.actvity.MainActivity
+import com.example.recify.actvity.MealActivity
 import com.example.recify.databinding.FragmentMealBottomSheetBinding
 import com.example.recify.viewModel.HomeViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -43,7 +46,26 @@ class MealBottomSheetFragment : BottomSheetDialogFragment() {
             viewModel.getMealById(it)
         }
         observeBottomSheetMeal()
+
+        onBottomSheetDialogClick()
     }
+
+    private fun onBottomSheetDialogClick() {
+        binding.bottomSheet.setOnClickListener {
+            if(mealName != null && mealThumb !=null){
+                val intent = Intent(activity,MealActivity::class.java)
+                intent.apply {
+                    putExtra(HomeFragment.MEAL_ID, mealId)
+                    putExtra(HomeFragment.MEAL_NAME,mealName)
+                    putExtra(HomeFragment.MEAL_THUMB,mealThumb)
+                }
+                startActivity(intent)
+            }
+        }
+    }
+    private var mealName : String?=null
+    private var mealThumb : String?=null
+
     private fun observeBottomSheetMeal(){
         viewModel.observeBottomSheetMeal().observe(viewLifecycleOwner){meal ->
             Glide.with(this)
@@ -52,6 +74,9 @@ class MealBottomSheetFragment : BottomSheetDialogFragment() {
             binding.bsLocation.text = meal.strArea
             binding.bsMealCategory.text = meal.strCategory
             binding.tvMealName.text = meal.strMeal
+
+            mealName = meal.strMeal
+            mealThumb = meal.strMealThumb
         }
     }
     companion object {
